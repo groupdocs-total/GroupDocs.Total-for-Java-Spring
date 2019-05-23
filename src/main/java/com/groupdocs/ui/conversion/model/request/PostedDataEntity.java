99af2a8e6e9922e1d2f64db1286973f422e0fb61
Pavel Egorov <egorovpasha@gmail.com>
2019-05-23 17:08:43 +0400

    Groupdocs.Conversion release(#60)

---
+++ src/main/java/com/groupdocs/ui/conversion/model/request/PostedDataEntity.java
@@ -0,0 +1,43 @@
+package com.groupdocs.ui.conversion.model.request;
+
+import java.util.List;
+
+public class PostedDataEntity {
+
+    private String path;
+    private String guid;
+    private String password;
+    private String url;
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getGuid() {
+        return guid;
+    }
+
+    public void setGuid(String guid) {
+        this.guid = guid;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
