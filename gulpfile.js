8fa186b13eec5fc07eceaa4e539f8731f5814a0e
Elena Kolesnikova <koleslena@yandex.ru>
2019-05-29 22:59:45 +0600

    added configs for angular

--- gulpfile.js
+++ gulpfile.js
@@ -4,4 +4,10 @@ gulp.task('build', function() {
   return gulp
     .src('./node_modules/@groupdocs.examples.jquery/**')
     .pipe(gulp.dest('./src/main/resources/static/assets/'))
+});
+
+gulp.task('copy', function() {
+  return gulp
+      .src('./node_modules/@groupdocs.total-angular/viewer/dist/**')
+      .pipe(gulp.dest('./src/main/resources/static/'))
 })
