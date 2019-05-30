b3f14014e7ea296b76ce5ad5bf41375f9935a797
Elena Kolesnikova <koleslena@yandex.ru>
2019-05-30 17:24:51 +0600

    fixed build

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,12 @@ gulp.task('build', function() {
 
 gulp.task('copy', function() {
   return gulp
-      .src('./node_modules/@groupdocs.examples.angular/viewer/dist/**')
-      .pipe(gulp.dest('./src/main/resources/static/'))
+      .src('./node_modules/@groupdocs.examples.angular/**')
+      .pipe(gulp.dest('./src/main/resources/static/angular/'))
+});
+
+gulp.task('config', function() {
+  return gulp
+      .src('./node_modules/@groupdocs.examples.angular/viewer/dist/assets/config/**')
+      .pipe(gulp.dest('./src/main/resources/static/assets/config/'))
 });
