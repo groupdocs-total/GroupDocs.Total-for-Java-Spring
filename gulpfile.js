fb131a9dbb86424c3c8bcca730f6137cdb2b8902
Elena Kolesnikova <koleslena@yandex.ru>
2019-07-17 18:13:30 +0600

    added client application

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,17 @@
 var gulp = require('gulp')
 
-gulp.task('build', function() {
+gulp.task('copy', function() {
   return gulp
     .src('./node_modules/@groupdocs.examples.jquery/**')
     .pipe(gulp.dest('./src/main/resources/static/assets/'))
 });
 
-gulp.task('copy', function() {
-  return gulp
-      .src('./node_modules/@groupdocs.examples.angular/**')
-      .pipe(gulp.dest('./src/main/resources/static/angular/'))
+var exec = require('child_process').exec;
+
+gulp.task('build', function (cb) {
+  exec('npm run build-client', function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    cb(err);
+  });
 });
