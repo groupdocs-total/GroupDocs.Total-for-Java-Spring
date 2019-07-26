316507c8f0a62e30ff6cd8897264b14e94ae8e95
Elena Kolesnikova <koleslena@yandex.ru>
2019-07-26 20:10:08 +0600

    added load config

--- src/main/java/com/groupdocs/ui/comparison/ComparisonController.java
+++ src/main/java/com/groupdocs/ui/comparison/ComparisonController.java
@@ -39,6 +39,13 @@ public class ComparisonController {
     @Autowired
     private ComparisonService comparisonService;
 
+
+    @RequestMapping(method = RequestMethod.GET, value = "/loadConfig", produces = APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public ComparisonConfiguration loadConfig() {
+        return comparisonService.getComparisonConfiguration();
+    }
+
     /**
      * Get comparison page
      *
