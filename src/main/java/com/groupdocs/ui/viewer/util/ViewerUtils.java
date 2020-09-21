0153bb6bbeb42d801da15a5b3673e8674d2b10ef
aleksey.permyakov <aleksey.permyakov@aspose.com>
2020-09-21 15:47:39 +0300

    Fixed media type detection

--- src/main/java/com/groupdocs/ui/viewer/util/ViewerUtils.java
+++ src/main/java/com/groupdocs/ui/viewer/util/ViewerUtils.java
@@ -1,68 +1,21 @@
 package com.groupdocs.ui.viewer.util;
 
-import com.groupdocs.ui.exception.TotalGroupDocsException;
 import com.groupdocs.viewer.Viewer;
 import com.groupdocs.viewer.options.ViewInfoOptions;
 import com.groupdocs.viewer.results.Page;
 import com.groupdocs.viewer.results.ViewInfo;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.URLConnection;
-import java.nio.file.Files;
 import java.util.List;
 
 public class ViewerUtils {
-    private static final Logger logger = LoggerFactory.getLogger(ViewerUtils.class);
 
     public static MediaType detectMediaType(String fileName) {
-        String mediaType = null;
-        try {
-            if (fileName.contains(".")) {
-                final String extension = fileName.substring(fileName.lastIndexOf("."));
-                switch (extension) {
-                    case ".otf":
-                        mediaType = "font/otf";
-                        break;
-                    case ".sfnt":
-                        mediaType = "font/sfnt";
-                        break;
-                    case ".ttf":
-                        mediaType = "font/ttf";
-                        break;
-                    case ".woff":
-                        mediaType = "font/woff";
-                        break;
-                    case ".woff2":
-                        mediaType = "font/woff2";
-                        break;
-                    case ".eot":
-                        mediaType = "application/vnd.ms-fontobject";
-                        break;
-                    case ".js":
-                        mediaType = "application/javascript";
-                        break;
-                    default:
-                        mediaType = null;
-                }
-            }
-            if (mediaType == null) {
-                mediaType = Files.probeContentType(new File(fileName).toPath());
-                if (mediaType == null) {
-                    mediaType = URLConnection.guessContentTypeFromName(fileName);
-                }
-                if (mediaType == null) {
-                    return MediaType.APPLICATION_OCTET_STREAM;
-                }
-            }
-        } catch (IOException e) {
-            logger.warn("Can't detect content type using file name '" + fileName + "'");
-            throw new TotalGroupDocsException("Can't detect content type using file name '" + fileName + "'", e);
+        if (fileName.contains(".")) {
+            final String extension = fileName.substring(fileName.lastIndexOf("."));
+            return MediaType.parseMediaType(MediaTypes.detectMediaTypeForWeb(extension));
         }
-        return MediaType.parseMediaType(mediaType);
+        return MediaType.APPLICATION_OCTET_STREAM;
     }
 
     public static void applyWidthHeightFix(Viewer viewer, ViewInfo viewInfo) {
